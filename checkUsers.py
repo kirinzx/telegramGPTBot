39612c91ed960ca2a7b69d5f3e5761d866995552
kirin <kirin3243@gmail.com>
2023-07-06 01:05:54 +0400

    commenting rdy, some bot fitures needed

--- checkUsers.py
+++ checkUsers.py
@@ -2,11 +2,14 @@ from telethon import TelegramClient
 import asyncio
 from telethon import events
 from chatGPTReq import req
+import random
+import sqlite3
+import threading
 
-def startParsing():
+def startParsing(session,app_id,app_hash):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    client = TelegramClient(session=".", api_id=1, api_hash=".",loop=loop)
+    client = TelegramClient(session=f"sessions/{session}", api_id=int(app_id), api_hash=app_hash,loop=loop)
     client.loop.run_until_complete(getChannels(client))
     return client
 
@@ -20,9 +23,21 @@ async def checkPosts(client:TelegramClient,channel):
     @client.on(events.NewMessage(chats=channel.id))
     async def handler(event):
         message = await req(event.text)
+        await asyncio.sleep(random.randint(120,300))
         await client.send_message(entity=channel,message=message,comment_to=event)
 
-def main():
-    client = startParsing()
+def getUsersClient(session,app_id,app_hash):
+    client = startParsing(session,app_id,app_hash)
     client.start()
     client.run_until_disconnected()
+
+def main():
+    userThreads = []
+    connection = sqlite3.connect("accounts.db")
+    cursor = connection.cursor()
+    for row in cursor.execute("SELECT phoneNumber,app_id,app_hash FROM users"):
+        userThreads.append(threading.Thread(target=getUsersClient,args=(row[0],row[1],row[2])))
+    for thread in userThreads:
+        thread.start()
+    for thread in userThreads:
+        thread.join()
--- telegramBot.py
+++ telegramBot.py
@@ -16,8 +16,9 @@ storage = MemoryStorage()
 bot = Bot(token=os.environ.get("API_TOKEN"))
 dp = Dispatcher(bot,storage=storage)
 
-keyboardMenu = ReplyKeyboardMarkup()
-keyboardMenu.add("На главную")
+keyboardCancel = ReplyKeyboardMarkup(keyboard=[
+    ["Отменить"],
+])
 keyboardMain = ReplyKeyboardMarkup(keyboard=[
     ["Посмотреть добавленные аккаунты","Добавить аккаунт"],
     ["Как я работаю?"]
@@ -44,22 +45,31 @@ async def getAccounts(message:types.Message):
             accounts = await cursor.fetchall()
     if len(accounts) > 0:
         accountsStr = "\n".join([nickname[0] for nickname in accounts])
-        await message.answer(text=accountsStr,reply_markup=keyboardMenu)
+        await message.answer(text=accountsStr,reply_markup=keyboardMain)
     else:
-        await message.answer(text="Аккаунтов нет...",reply_markup=keyboardMenu)
+        await message.answer(text="Аккаунтов нет...",reply_markup=keyboardMain)
 
 @dp.message_handler(Text(equals="Добавить аккаунт"))
 async def addAccount(message:types.Message):
     await UserForm.phoneNumber.set()
-    await message.answer(text="Напишите номер телефона аккаунт(с кодом страны):",reply_markup=keyboardMenu)
+    await message.answer(text="Напишите номер телефона аккаунт(с кодом страны):",reply_markup=keyboardCancel)
 
 @dp.message_handler(Text(equals="Как я работаю?"))
 async def getHelp(message:types.Message):
-    await message.answer(text="Добавьте аккаунты и я начну комментировать их посты:",reply_markup=keyboardMenu)
+    await message.answer(text="Добавьте аккаунты и я начну комментировать их посты:",reply_markup=keyboardMain)
 
 @dp.message_handler(Text(equals="На главную"))
 async def goHome(message:types.Message):
     await message.answer(text="Выберите опцию",reply_markup=keyboardMain)
+
+@dp.message_handler(state='*', commands='Отменить')
+@dp.message_handler(Text(equals='Отменить'), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('Отменено', reply_markup=keyboardMain)
 
 @dp.message_handler(state=UserForm.phoneNumber)
 async def process_nickname(message: types.Message, state: FSMContext):
@@ -67,7 +77,7 @@ async def process_nickname(message: types.Message, state: FSMContext):
         data['phoneNumber'] = message.text
 
     await UserForm.next()
-    await message.reply("Напишите api_id")
+    await message.reply("Напишите api_id",reply_markup=keyboardCancel)
 
 @dp.message_handler(state=UserForm.app_id)
 async def process_app_id(message: types.Message, state: FSMContext):
@@ -75,7 +85,7 @@ async def process_app_id(message: types.Message, state: FSMContext):
         data['app_id'] = message.text
 
     await UserForm.next()
-    await message.reply("Напишите api_hash")
+    await message.reply("Напишите api_hash",reply_markup=keyboardCancel)
 
 @dp.message_handler(state=UserForm.app_hash)
 async def process_app_hash(message: types.Message, state: FSMContext):
@@ -88,11 +98,11 @@ async def process_app_hash(message: types.Message, state: FSMContext):
                 await db.commit()
             global client, user
             user = User(userTMP[0],userTMP[1],userTMP[2])
-            client = TelegramClient(session=data["phoneNumber"],api_id=data["app_id"],api_hash=data["app_hash"])
+            client = TelegramClient(session=f'sessions/{data["phoneNumber"]}',api_id=data["app_id"],api_hash=data["app_hash"])
             await client.connect()
             if not await client.is_user_authorized():
                 await client.send_code_request(phone=data["phoneNumber"])
-                await message.answer("Введите код, который пришел вам в телеграм!", reply_markup=keyboardMenu)
+                await message.answer("Введите код, который пришел вам в телеграм!",reply_markup=keyboardCancel)
                 await state.finish()
                 await AccessCodeForm.code.set()
             else:
