f157daf7738d4a06df314b1b03f59dbd29807fe5
kirin <kirin3243@gmail.com>
2023-12-20 00:35:50 +0400

    fixed bug with chatgpt

--- bot/chatGPTReq.py
+++ bot/chatGPTReq.py
@@ -2,12 +2,13 @@ from openai import AsyncOpenAI
 from config import getSetting
 import httpx
 
+
 async def req(post):
     try:
         proxy = getSetting('proxy')
         if proxy:
             openai = AsyncOpenAI(
-                api_key = getSetting('openai_api_key'),
+                api_key=getSetting('openai_api_key'),
                 http_client=httpx.AsyncClient(
                     proxies="http://" + getSetting('proxy'),
                 )
@@ -17,8 +18,8 @@ async def req(post):
                 api_key=getSetting('openai_api_key')
             )
         prompt = getSetting('chatGPTRequest')
-        request = prompt.replace("$$",str(post).strip(),1)
-        response = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo",messages=[{"role":"user","content":request}])
+        request = prompt.replace("$$", str(post).strip(), 1)
+        response = await openai.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": request}])
         return response.choices[0].message.content.strip()
     except Exception as e:
         print(e)
